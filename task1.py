e984df23f5b582e15041c808d01f4cd4b8d5d70d
ciwwan1980 <ajilqado@gmail.com>
2023-08-29 15:02:24 +0200

    solution with-only-if

--- task1.py
+++ task1.py
@@ -5,19 +5,34 @@
 # - print 'buzz' for multiples of 5, 
 # - print 'fizzbuzz' for multiples of 3 and 5.
 
-three_mul = "fizz"
-five_mul = 'buzz'
-num1 = 3
-num2 = 5 
-max_num = 100
+# three_mul = "fizz"
+# five_mul = 'buzz'
+# num1 = 3
+# num2 = 5 
+# max_num = 100
    
-for i in range(1,max_num+1):
+# for i in range(1,max_num+1):
     
-    # first condition shall include both num and num2,  otherwise , if i put the order not like this, it will never go to this condition, because it will always be true  
-    if (i%num1 == 0 and i%num2 == 0):
-        print(i, three_mul+five_mul) 
+#     # first condition shall include both num and num2,  otherwise , if i put the order not like this, it will never go to this condition, because it will always be true  
+#     if (i%num1 == 0 and i%num2 == 0):
+#         print(i, three_mul+five_mul) 
 
-    elif i%num2 == 0:
-        print(i, five_mul)
-    elif  i %num1 == 0:
-        print(i, three_mul)   
+#     elif i%num2 == 0:
+#         print(i, five_mul)
+#     elif  i %num1 == 0:
+#         print(i, three_mul)   
+
+max_num = 100
+
+for i in range(1, max_num + 1):
+    output = ""
+# i put all condition with if statement, otherwise will not work with elif, we ensure that only one of the two conditions is met for each number
+    if i % 3 == 0:
+        output += "fizz"
+    elif i % 5 == 0:
+        output += "buzz"
+    
+    if output == "":
+        output = i
+    
+    print(output)
