0871515d189d818228eaaff54458d9b9ceb97cc4
ciwwan1980 <ajilqado@gmail.com>
2023-08-29 15:30:53 +0200

    task4

---
+++ task4.py
@@ -0,0 +1,11 @@
+# Your task is to fix program non-working correctly.
+# The problem:  
+#   - there are 4 short programs with loops, that should print some numbers, but they don't work at all!  
+
+#- 1
+for x in range(3):
+    print(x)
+
+#- 2
+for j in range(5):
+    print(f"This is loop number {j}")
