91996fa71a065177f9eb9950f9a39da7f97d8918
ciwwan1980 <ajilqado@gmail.com>
2023-08-29 15:34:19 +0200

    task4-3-4

--- task4.py
+++ task4.py
@@ -7,5 +7,24 @@ for x in range(3):
     print(x)
 
 #- 2
+
 for j in range(5):
     print(f"This is loop number {j}")
+
+#- 3
+
+x=10
+while x > 0:
+    print(x)
+    x -= 1
+    
+x = 10
+
+# -4
+
+countdown=5
+while countdown:
+    print(f"{countdown}")
+    countdown -= 1
+else:
+    print("Start!")
